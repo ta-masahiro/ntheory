493dbe4b376fd8a45ebac9fe8b4d979c1e204e97
ta-masahiro <ta.masahiro@gmail.com>
2021-09-12 13:28:03 +0900

    歩ラードp-1法追加 参考でmathlab.pyのpython3版追加

---
+++ pollard_pm1.c
@@ -0,0 +1,35 @@
+ //   Factorization_pollardsP_1 : ポラードの p-1 法を使い素因数分解を行う
+ //   n 素因数分解を行う対象の数
+ //   g 発見された因数
+ //   b 底
+ //   maxC 最大処理回数(指数の最大値 + 1)
+ //   戻り値 : 素因数分解に成功したらTRUEを返す
+int pollard_pm1(mpz_t n, mpz_t g, unsigned long c, unsigned long maxC ) {
+    mpz_t m; mpz_set_init_ui(m, b); 
+    mpz_t mm1; mpz_init(mm1); 
+    mpz_t g; mpz_init(g); 
+
+    for ( unsigned long c = 0 ; c < maxC ; ++c ) {
+        mpz_powm_ui(m, m, c + 1, n); 
+        mpz_sub_ui(mm1, m, 1); 
+        mpz_gcd(g, mm1, n); 
+        if (mpz_cmp_ui(g, 1) == 0) continue; 
+        if (mpz_cmp(g, n) == 0) return FALSE; 
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int main(int argc, char * argv[]) {
+    mpz_t  d; mpz_init(d);
+    mpz_t  n; mpz_init_set_str(n, argv[1], 10);
+    int b=atoi(argv[2]);
+    int maxC=atoi(argv[3]);
+    if (mpz_probab_prime_p (n,20)) 
+        printf("%s is prime\n",mpz_get_str(NULL,10,n));
+    //else if (pollard_rho(n,d)) {
+    else if (pollard_pm1(n,d,b,maxE)) {
+        mpz_tdiv_q(n,n,d);
+        printf("%s is  %s * %s\n",  argv[1], mpz_get_str(NULL,10,d), mpz_get_str(NULL,10,n));
+    } else printf("pollard_rho method fail!\n");
+}
