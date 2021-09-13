d18d30501c3da76639d5e227dc1b73d40b688851
ta-masahiro <ta.masahiro@gmail.com>
2021-09-13 10:14:27 +0900

    すべての素因数を表示

--- pollard_pm1.c
+++ pollard_pm1.c
@@ -13,9 +13,8 @@
  #define FALSE 0
 
 int pollard_pm1(mpz_t n, mpz_t g, unsigned long b, unsigned long maxC ) {
-    mpz_t m; mpz_set_init_ui(m, b); 
+    mpz_t m; mpz_init_set_ui(m, b); 
     mpz_t mm1; mpz_init(mm1); 
-    mpz_t g; mpz_init(g); 
 
     for ( unsigned long c = 0 ; c < maxC ; ++c ) {
         mpz_powm_ui(m, m, c + 1, n); 
@@ -31,13 +30,16 @@ int pollard_pm1(mpz_t n, mpz_t g, unsigned long b, unsigned long maxC ) {
 int main(int argc, char * argv[]) {
     mpz_t  d; mpz_init(d);
     mpz_t  n; mpz_init_set_str(n, argv[1], 10);
-    int b=atoi(argv[2]);
-    int maxC=atoi(argv[3]);
-    if (mpz_probab_prime_p (n,20)) 
-        printf("%s is prime\n",mpz_get_str(NULL,10,n));
-    //else if (pollard_rho(n,d)) {
-    else if (pollard_pm1(n, d, b, maxC)) {
-        mpz_tdiv_q(n,n,d);
-        printf("%s is  %s * %s\n",  argv[1], mpz_get_str(NULL,10,d), mpz_get_str(NULL,10,n));
-    } else printf("pollard p-1 method fail!\n");
+    int b=strtoul(argv[2], 0, 10);
+    int maxC=strtoul(argv[3], 0, 10);
+    while (mpz_probab_prime_p (n,20) == 0) {
+        if (pollard_pm1(n,d,b,maxC)) {
+            mpz_tdiv_q(n,n,d);
+            printf("%s ", mpz_get_str(NULL,10,d));
+        } else {
+            printf(" pollard p-1 method fail! in %s\n", mpz_get_str(NULL,10,n));
+            return 0;
+        }    
+    }
+    printf("%s\n",mpz_get_str(NULL,10,n));
 }
